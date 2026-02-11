d0d8a6118a729975f68ea981400f9978f25fbd44
Piyush1532 <piyushadav503@gmail.com>
2026-02-11 12:38:57 +0530

    implemented sd model

--- src/Components/ProductViewer.jsx
+++ src/Components/ProductViewer.jsx
@@ -4,25 +4,31 @@ import clsx from 'clsx'
 import {Canvas} from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei";
 import MacbookModel14 from './models/Macbook-14';
-import StudioLights from './StudioLights';
+import StudioLights from './three/StudioLights';
+import ModelSwitcher from './three/ModelSwitcher';
+import { useMediaQuery } from 'react-responsive';
+
+
 const ProductViewer = () => {
 const {color,scale,setColor,setScale}=useMacBookStore()
+
+const isMobile=useMediaQuery({ query:"(max-width:1024px)"})
 
 
   return (
     <section id='product-viewer'>
         <h2>Take a closer look</h2>
         <div className="controls mt-20">
-            <p className='info'>Macbook Pro 16 Space Black</p>
+            {/* <p className='info'>Macbook Pro | Available in 14* & 16"|in {color}</p> */}
 
-            <div className='flex-center gap-5 mt-10'>
+            <div className='flex flex-center gap-5 mt-10'>
                 <div className="color-control">
 <div
   className={clsx(
     "bg-neutral-300",
-    color === "#abe5bd" && "active"
+    color === "#C4C4C5" && "active"
   )}
-  onClick={() => setColor("#abe5bd")}
+  onClick={() => setColor("#C4C4C5")}
 />
 <div
   className={clsx(
@@ -32,7 +38,7 @@ const {color,scale,setColor,setScale}=useMacBookStore()
   onClick={() => setColor("#2e2c2e")}
 />
 </div>
-</div>
+
 <div className="size-control">
 <div
   className={clsx(scale===0.06 ?"bg-white text-black":"bg-transparent text-white")}
@@ -46,13 +52,15 @@ const {color,scale,setColor,setScale}=useMacBookStore()
  
 </div>
 
+</div>
+
+
                 </div>
 
             <Canvas id="canvas" camera={{position:[0,2,5],fov:50,near:0.1,far:100}}>
           <StudioLights/>
-<MacbookModel14 scale={0.06} position={[0,0,0]}/>
-
-<OrbitControls enableZoom={false}/>
+{/* <MacbookModel14 scale={0.06} position={[0,0,0]}/> */}
+<ModelSwitcher  scale={isMobile?scale-0.03:scale} isMobile={isMobile}/>
             </Canvas>
     </section>
   )
